7d26a9a1cae181b117ef53d11cb3c0249f89c396
Danilo Panahon <danpan@jpj.com>
2021-07-02 18:28:39 +0800

    deleted files

--- node_modules/.cache/nuxt/dist/client/b292bdf.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{207:function(o,e){o.exports={attributes:{slug_name:"assets data",promo_banner:{promo_images:{promo_bg_desktop:"aomine.jpg",promo_bg_mobile:"aomine.jpg",promo_bg_banner:"aomine.jpg"},promo_title:"sadadasd"}},html:""}}}]);
